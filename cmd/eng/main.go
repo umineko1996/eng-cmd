830c23241c25d21eb6d690e32e1288e8c55e78e2
umineko1996 <umineko1996@gmail.com>
2019-04-24 21:31:54 +0900

    機能追加_enogu部分をカラー表示できるように変更

--- cmd/eng/main.go
+++ cmd/eng/main.go
@@ -9,14 +9,46 @@ import (
 )
 
 var out io.Writer = os.Stdout
-var EnoguAA = loadAA("data/enogu_logo_AA.txt")
+var enoguAA = loadAA("data/enogu_logo_AA.txt")
 
 func main() {
+	printLogoAA()
+}
+
+func printLogoAA() {
 	fmt.Fprintln(out, "\x1b[2J")
 	for i := 50; i > 0; i-- {
-		for j, line := range strings.Split(EnoguAA, "\r\n") {
+		for j, line := range strings.Split(enoguAA, "\r\n") {
 			fmt.Fprintf(out, "\x1b[%d;%dH\x1bK", j+5, i)
-			fmt.Println(line, " ")
+			if j < 18 {
+				fmt.Println(line, " ")
+				continue
+			}
+			x := 0
+			for _, r := range line {
+				var color int
+				if isWidth(r) {
+					x += 2
+				} else {
+					x++
+				}
+				switch {
+				case x < 11:
+					color = 0
+				case x < 21:
+					color = 31
+				case x < 31:
+					color = 35
+				case x < 43:
+					color = 36
+				case x < 54:
+					color = 32
+				default:
+					color = 33
+				}
+				fmt.Fprintf(out, "\x1b[%dm%s", color, string(r))
+			}
+			fmt.Println(" ")
 		}
 		time.Sleep(50 * time.Millisecond)
 	}
@@ -25,4 +57,12 @@ func main() {
 func loadAA(filename string) string {
 	b := MustAsset(filename)
 	return string(b)
+}
+
+func isWidth(r rune) bool {
+	if r <= 0xF0 {
+		// ASCII: size 1
+		return false
+	}
+	return true
 }
